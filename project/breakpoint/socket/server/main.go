dba0190862c8996bebae62753b139c9f895710a5
bxm426 <h12345678>
2021-03-01 20:56:09 +0800

    断点续传tcp

--- project/breakpoint/socket/server/main.go
+++ project/breakpoint/socket/server/main.go
@@ -1,1 +1,47 @@
-package server
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func serverConn(conn net.Conn) error {
+	return nil
+}
+
+func main() {
+	// 建立监听
+	l, err := net.Listen("tcp", ":8888")
+	if err != nil {
+		log.Fatalf("error listen")
+	}
+
+	defer l.Close()
+
+	log.Println("waiting accept.")
+
+	var errChan = make(chan error)
+
+	go func() {
+		for {
+			conn, err := l.Accept()
+			if err != nil {
+				log.Fatalf("accept faild: %s\n", err)
+				errChan <- err
+			}
+			serverConn(conn)
+		}
+	}()
+
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+		errChan <- fmt.Errorf("%s", <-c)
+	}()
+
+	log.Fatalf("server out err: %d", <-errChan)
+}
