7b930b0c8e767b50643b934a13b17ee9bea9ef1c
bxm426 <h123456>
2021-03-02 01:44:46 +0800

    断点续传 连接客户端,发送当前位置 接收完毕，断开连接

--- project/breakpoint/socket/client/main.go
+++ project/breakpoint/socket/client/main.go
@@ -1,1 +1,86 @@
 package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+	"strconv"
+	"time"
+)
+
+func clientRead(conn net.Conn) int {
+	buf := make([]byte, 5)
+	n, err := conn.Read(buf)
+	if err != nil {
+		log.Fatalf("receive server info faild:%s \n", err)
+	}
+
+	// string conver int
+	off, err := strconv.Atoi(string(buf[:n]))
+	if err != nil {
+		log.Fatalf("string conver int faild:%s\n", err)
+	}
+	return off
+}
+
+func clientWrite(conn net.Conn, data []byte) {
+	_, err := conn.Write(data)
+	if err != nil {
+		log.Fatalf("send 【%s】 content faild: %s\n", string(data), err)
+	}
+	log.Printf("send 【%s】 content success\n", string(data))
+}
+
+func clientConn(conn net.Conn) {
+	defer conn.Close()
+
+	// 发送 "start-->"消息通知服务端，我要开始发送文件内容了
+	// 获取服务端的通知，得知已经接收到了多少内容，从已经接收的内容处开始继续发送
+	clientWrite(conn, []byte("start-->"))
+	off := clientRead(conn)
+
+	// send file content
+	fp, err := os.OpenFile("test.txt", os.O_RDONLY, 0755)
+	if err != nil {
+		log.Fatalf("open file faild: %s\n", err)
+	}
+	defer fp.Close()
+
+	// 设置从那儿开始读取文件
+	_, err = fp.Seek(int64(off), 0)
+	if err != nil {
+		log.Fatalf("set file seek faild: %s\n", err)
+	}
+
+	log.Printf("read file at seek: %d\n", off)
+
+	// start send
+	for {
+		// 每次发送10个字节大小的内容
+		data := make([]byte, 10)
+		n, err := fp.Read(data)
+		if err != nil {
+			if err == io.EOF {
+				// 如果已经读取完 文件内容
+				// 就发送 '<--end' 消息通知服务端， 文件内容发送完了
+				time.Sleep(time.Second * 1)
+				clientWrite(conn, []byte("<-- end"))
+				log.Println("send all content, now quit")
+				break
+			}
+			log.Fatalf("read file err: %s\n", err)
+		}
+		// 发送文件内容到服务端
+		clientWrite(conn, data[:n])
+	}
+}
+
+func main() {
+	// connect timeout 10s
+	conn, err := net.DialTimeout("tcp", ":8888", time.Second*10)
+	if err != nil {
+		log.Fatalf("client dial faild: %s\n", err)
+	}
+	clientConn(conn)
+}
--- project/breakpoint/socket/server/main.go
+++ project/breakpoint/socket/server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"io"
 	"net"
+	"strconv"
 )
 
 /*
@@ -15,13 +16,42 @@ import (
 
 import (
 	"fmt"
-	"io"
 	"log"
-	"net"
 	"os"
 	"os/signal"
 	"syscall"
 )
+
+// 把接收到的内容 写入文件
+func writeFile(content []byte) {
+	if len(content) != 0 {
+		fp, err := os.OpenFile("test_1.txt", (os.O_CREATE | os.O_WRONLY | os.O_APPEND), 0755)
+		defer fp.Close()
+		if err != nil {
+			log.Fatal("open file faild: %s\n", err)
+		}
+		_, err = fp.Write(content)
+		if err != nil {
+			log.Fatal("append content to file faild: %s\n", err)
+		}
+		log.Printf("append content: 【%s】 success\n", string(content))
+	}
+}
+
+// 获取已接受的内容的大小
+// 断点续传需要把已接收内容大小 通知客户端从哪儿开始发送文件内容
+func getFileStat() int64 {
+	fileInfo, err := os.Stat("test_1.txt")
+	if err != nil {
+		if os.IsNotExist(err) {
+			log.Printf("file size: %d\n", 0)
+			return int64(0)
+		}
+		log.Fatalf("get file stat faild: %s\n", err)
+	}
+	log.Printf("file size: %d\n", fileInfo.Size())
+	return fileInfo.Size()
+}
 
 func serverConn(conn net.Conn) {
 	for {
@@ -36,6 +66,23 @@ func serverConn(conn net.Conn) {
 		}
 
 		log.Printf("recevice %d bytes, content is 【%s】\n", n, string(buf[:n]))
+
+		switch string(buf[:n]) {
+		case "start-->":
+			off := getFileStat()
+
+			stringoff := strconv.FormatInt(off, 10)
+			_, err := conn.Write([]byte(stringoff))
+			if err != nil {
+				log.Fatalf("server write faild: %s\n", err)
+			}
+			continue
+		case "<--end":
+			// 如果接收到客户端同志所有文件内容发送完毕消息则退出
+			log.Fatalf("receive over \n")
+			return
+		}
+		writeFile(buf[:n])
 	}
 }
 
