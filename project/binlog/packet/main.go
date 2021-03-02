f741fe3b46cd0b2acc30c8351be661d5bbb15154
bxm426 <h12345678>
2021-03-02 20:53:14 +0800

    new

---
+++ project/binlog/packet/main.go
@@ -0,0 +1,43 @@
+package packet
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
+
+func main() {
+	// 连接mysql
+	conn, err := connMysql("127.0.0.1:3306", "root", "password")
+	if err != nil {
+		fmt.Println("connMysql-err", err)
+		os.Exit(1)
+	}
+	// 进行握手 HandShake协议
+	handshake(conn)
+	// show global variables like 'binlog_checksum' 返回checksum 类型
+
+	// show master status 返回binlogFileName, Position
+
+	// 发送 binlog dump 命令
+
+	// 循环读取binlog event 流
+}
+
+func connMysql(addr string, user string, password string) (*Conn, error) {
+	conn, err := net.DialTimeout("tcp", addr, 10*time.Second)
+	return NewConn(conn), err
+}
+
+func handshake(conn *Conn) error {
+
+	data, err := conn.ReadPacket()
+	if err != nil {
+		fmt.Println("err", err)
+		return err
+	}
+
+	fmt.Println("data", data)
+	return nil
+}
